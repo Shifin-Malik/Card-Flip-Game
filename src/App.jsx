febfb4606e2f1dff0d1c745b4584a26b7b008efd
shifinmalik <shifinmalik@gmail.com>
2024-09-26 07:17:38 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import SingleCard from "./components/SingleCard";
+import beeImage from '/img/bee.png';
+import dinosaurImage from '/img/dinosaur.png';
+import elephantImage from '/img/elephant.png';
+import foxImage from '/img/fox.png';
+import frogImage from '/img/frog.png';
+import happyFaceImage from '/img/happy-face.png';
+import happy from '/img/happy.png';
+import monkey from '/img/monkey.png';
+import panda from '/img/panda.png';
+
+
+const cardImages = [
+  { src: beeImage, matched: false },
+  { src: dinosaurImage, matched: false },
+  { src: elephantImage, matched: false },
+  { src: foxImage, matched: false },
+  { src: frogImage, matched: false },
+  { src: happyFaceImage, matched: false },
+  { src: happy, matched: false },
+  { src: monkey, matched: false },
+  { src: panda, matched: false },
+ 
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  // Shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  // Handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // Compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) =>
+          prevCards.map((card) => 
+            card.src === choiceOne.src ? { ...card, matched: true } : card
+          )
+        );
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // Reset choices & increase turns
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  // Start a new game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  return (
+    <div className="h-screen w-full  bg-primary px-10">
+      <h1 className="text-4xl text-white font-bold text-center pt-4">Magic Match</h1>
+      <div className="flex justify-center pt-4 gap-3">
+        <button
+          onClick={shuffleCards}
+          className="bg-transparent border-2 border-white px-3 py-2 rounded text-white font-bold cursor-pointer text-base hover:bg-third"
+        >
+          New Game
+        </button>
+        <p className="text-lg text-white ">Turns: {turns}</p>
+      </div>
+
+      <div className="card-grid mt-10 grid grid-cols-3 gap-5">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+     'node_modules/flowbite-react/lib/esm/**/*.js'
+  ],
+  theme: {
+    extend: {
+      colors:{
+ primary:"#1b1523",
+ secondary:"#eff1f3",
+ third:"#C5056A",
+
+ 
+},
+    },
+  },
+ 
+
+}
